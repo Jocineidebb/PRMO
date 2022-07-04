28c26e0802eb10028276bd2563d6d9e0ffd6a7aa
Jocineidebb <103062765+Jocineidebb@users.noreply.github.com>
2022-07-04 13:49:25 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/perfil.dart';
-
+import 'package:mobile/landing_page.dart';
+import 'package:mobile/salutation_page.dart';
 
 void main() => runApp(MyApp());
